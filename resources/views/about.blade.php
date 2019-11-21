a02cf00794a0b931e1948fe3ba6b89ec2819daa5
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-11-21 12:54:10 +0700

    Persiapan menggunakan laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+@extends('template/main')
+
+@section('judul', 'About')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1>Hello, Zikri</h1>
+        </div>
+    </div>
+</div>
+@endsection
